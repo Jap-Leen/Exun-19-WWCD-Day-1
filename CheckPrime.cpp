e9d7c4027550da613afc9a2e8911aaeeae8571f7
anushreeberlia <56750712+anushreeberlia@users.noreply.github.com>
2019-10-19 13:24:41 +0530

    Update CheckPrime.cpp

--- CheckPrime.cpp
+++ CheckPrime.cpp
@@ -1,1 +1,15 @@
-// Given a number n, check whether it is a prime number or not.
+#include<iostream.h>
+void main()
+{
+     int n,i,c=0;
+    cin>>n;
+  for(i=1;i<=n;i++)
+  {
+    if(n%i==0)
+      c++;
+  }
+  if(c>2)
+    cout<<"no";
+  else
+    cout<<"yes";
+}
