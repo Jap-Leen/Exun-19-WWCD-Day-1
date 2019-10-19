c739ffbd2630c952f0236cec4dc69f2b6c001f17
shrutika11 <56750814+shrutika11@users.noreply.github.com>
2019-10-19 13:26:10 +0530

    count number of prime numbers in a range

--- PrimeNumbers.cpp
+++ PrimeNumbers.cpp
@@ -1,1 +1,28 @@
 // Given a number n, print the first 'n' prime numbers.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n,chk=0;
+  cout<<"enter the number";
+  cin>>n;
+  for(int i=2;i<n;i++){
+      chk=0;
+  
+    for(int j=2;j<i-1;j++)
+    {
+
+      if(i%j==0)
+      { 
+        chk=1;
+        break;
+      } 
+
+    }
+    if(chk==0)
+       cout<<i<<endl;
+  }
+    return 0;
+  } 
+  
