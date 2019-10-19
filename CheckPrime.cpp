ae6eb1a05645e6e0210cb10f13f40bc81d537cac
SK20137 <56750704+SK20137@users.noreply.github.com>
2019-10-19 13:04:03 +0530

    Solution to CheckPrime

--- CheckPrime.cpp
+++ CheckPrime.cpp
@@ -1,1 +1,26 @@
 // Given a number n, check whether it is a prime number or not.
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n = 0;
+	bool result = true;
+	cin>>n;
+    for(int i = 2; i < n; i++)
+    {
+        if(n%i == 0)
+        {
+            result = false;
+        }
+    }
+    if(result == true)
+    {
+        cout<< n << " is a prime number."<<endl;
+    }
+    else
+    {
+        cout<< n <<" is not a prime number."<<endl;
+    }
+	return 0;
+}
