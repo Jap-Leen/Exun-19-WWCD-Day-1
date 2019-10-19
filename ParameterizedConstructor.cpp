939d0aeb1eea75baeacd44e7e7004876e58b24b3
shrutika11 <56750814+shrutika11@users.noreply.github.com>
2019-10-19 14:06:33 +0530

    Update ParameterizedConstructor.cpp

--- ParameterizedConstructor.cpp
+++ ParameterizedConstructor.cpp
@@ -1,1 +1,29 @@
 Write a class implementing parameterized constructor.
+  class train
+  {
+    int seat;
+    int age;
+    
+    public:
+    train(st=1,ag=4)
+    {
+      seat=st;
+      age=ag;
+    }
+    
+    void showd()
+    {
+      cout<<seat;
+      cout<<age;
+    } 
+  };
+
+  int main()
+  {
+    train t;
+    t.showd();
+    return 0;
+  
+  } 
+    
+    
