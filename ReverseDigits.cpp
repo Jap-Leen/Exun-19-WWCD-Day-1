f9a61513eea954e1dfd2907afdb9e6b21ccd4861
aagupta297 <56750710+aagupta297@users.noreply.github.com>
2019-10-19 22:43:35 +0530

    Added code for Reversing digits of a number (c++)

--- ReverseDigits.cpp
+++ ReverseDigits.cpp
@@ -1,2 +1,25 @@
 // Given a number n, reverse its digits and print.
+#include<iostream>
 
+using namespace std;
+
+int rev_digits(int n)
+{
+    int reverse=0,rem;
+    while(n!=0)    
+  {    
+     rem=n%10;      
+     reverse=reverse*10+rem;    
+     n/=10;    
+  }    
+  cout<<" The reversed number is: "<<reverse;
+}
+
+int main()
+{
+  int n;
+  cout<<"Enter a number: ";
+  cin>>n;
+  rev_digits(n);
+  return 0;
+}
