e837aa92c384ce8fdfafa6124af8208c405a6281
ridhisehgal <56750672+ridhisehgal@users.noreply.github.com>
2019-10-19 13:29:07 +0530

    Program for InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,2 +1,16 @@
 
 #Given n numbers sort them using Insertion Sort.
+l=list()
+n=int(input("Enter the no. of elements"))
+for x in range(n):
+  a=int(input("Enter an element"))
+  l.append(a)
+print("The original list is",l)
+for i in range(1,n):
+  key=l[i]
+  j=i-1
+  while (j>=0 and key<l[j]):
+        l[j+1]=l[j]
+        j-=1
+  l[j+1]=key
+print("The sorted list is",l)
