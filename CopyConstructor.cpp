b1ff4bc0fc57768bf7a25cb504cd6dced13164f0
anushreeberlia <56750712+anushreeberlia@users.noreply.github.com>
2019-10-19 13:30:41 +0530

    Update CopyConstructor.cpp

--- CopyConstructor.cpp
+++ CopyConstructor.cpp
@@ -1,1 +1,22 @@
 Write a class implementing copy constructor.
+  #include <iostream>
+using namespace std;
+class abc
+{
+  int a;
+  float b;
+  public:
+  abc()
+  {
+    a=0;
+    b=0.0;
+  }
+  abc(abc &t)
+  {a=t.a;
+  b=t.b;}
+};
+int main() {
+   abc obj,obj1(obj);
+    return 0;
+
+}
