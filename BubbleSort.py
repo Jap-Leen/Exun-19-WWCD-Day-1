946b0543cc9676b11acc8ed2484b975c30f2ee32
himaniagarwal04 <56750680+himaniagarwal04@users.noreply.github.com>
2019-10-19 13:04:45 +0530

    update bubblesort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,2 +1,12 @@
-
 #Given n numbers sort them using Bubble Sort.
+n=eval(input("Enter how many numbers"))
+a=[]
+for i in range(1,n+1):
+  num=eval(input("Enter the number"))
+  a.append(num)
+  #Sorting
+  for i in range(0,len(a)):
+    for j in range(0,len(a)-1):
+      if a[j]>a[j+1]:
+        a[j],a[j+1]=a[j+1],a[j]
+print(a)
