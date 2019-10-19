35857b0ee43569c579ebc7c126a4154ad2a137d4
muskanvaswan <42072661+muskanvaswan@users.noreply.github.com>
2019-10-19 13:21:32 +0530

    Update Palindrome.cpp
    palindrome program

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,1 +1,42 @@
 // Given a number n, check whether it is a palindrome or not.
+#include <iostream>
+using namespace::std;
+
+int count(int n)
+{
+  int i;
+  for(i=0; n/10!=0;i++)
+  return i;
+}
+    
+int main()
+{
+  int n,n1,p,x[50], flag=0;
+  cout<<"enter number you want to check:";
+  cin>>n;
+  n1=n;
+  p = count(n);
+  for(int i=1;i<=p;i++)
+  {
+    x[p-i]=n%10;
+    n=n/10;
+  }
+  for(int i=0;i<p/2;i++)
+  {
+    if(x[i]!=x[p-i])
+    {
+      cout<<"number is not a palindrome";
+      flag=1;
+      break;
+    }
+    else
+      continue;
+  }
+  if(flag==0)
+    cout<<"number is a palindrome";
+  return 0;
+}
+    
+  
+  
+  
