4e15627e8c5713a117d7a995d075be6085de273c
laavanayadhawan <48582727+laavanayadhawan@users.noreply.github.com>
2019-10-19 13:19:30 +0530

    program illustrating destructor in c++

--- Destructor.cpp
+++ Destructor.cpp
@@ -1,1 +1,20 @@
 Write a class implementing destructor function.
+  #include<iostream>
+#include<fstream>
+using namespace std;
+class A{
+        int x;
+        public:
+        A() //constructor
+        {
+            cout<<"constructor invoked.";
+        }
+        ~A()
+        {
+            cout<<"destructor invoked.";
+        }
+};
+int main()
+{
+    A a;
+}    
