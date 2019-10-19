1dd186a082915ab7982860f1c790deedf326815d
aagupta297 <56750710+aagupta297@users.noreply.github.com>
2019-10-19 13:16:12 +0530

    Added int main() for input - binary search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,1 +1,21 @@
 // Given n numbers and a number k, write a program to search the number. k in the list using Binary Search algorithm.
+#include<iostream>
+
+int main()
+{ 
+    system("cls");
+  int n, a[10], k, m;
+  cout<<"Enter number of elements in the array (max. 10) (in ascending order) ";
+  cin>>n;
+ cout<<"Enter array elements: ";
+ for(int i=0; i<n; i++)
+   cin>>a[i];
+    cout<<"Enter element to be searched for";
+  cin>>n;
+ k=binary_search(a, n, m);
+ if(k==0)
+ cout<<"Element found!";
+ else
+ cout<<"Ëlement not found!";
+ return 0;
+}
