c610f2245f083b6c4f75f909b14cd39b0a5f74ed
himaniagarwal04 <56750680+himaniagarwal04@users.noreply.github.com>
2019-10-19 13:12:50 +0530

    Update CheckPrime.py

--- CheckPrime.py
+++ CheckPrime.py
@@ -1,1 +1,11 @@
 # Given a number n, check whether it is a prime number or not.
+n=int(input("Enter the no."))
+factor=0
+for i in range(2,n+1):
+  if n%i==0:
+    factor+=1
+if factor>=2:
+  print("The number is not prime")
+else:
+  print("The number is prime")
+    
